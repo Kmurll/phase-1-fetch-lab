a6546401a80913b13f96e14c676c232d228d3e83
Kelvin Muli Mue <kelvinmue@gmail.com>
2023-07-18 22:50:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+    return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => {
+      renderBooks(json); // Call the renderBooks function with the JSON data
+    })
+    .catch((error) => {
+      console.error("Error fetching books:", error);
+    });
 }
+  // To pass the tests, don't forget to return your fetch! 
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
